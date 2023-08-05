0ed497b3a7d5f686b46ce59a3500307c5a7b0ec4
OlenaDiahil <122643832+OlenaDiahil@users.noreply.github.com>
2023-08-05 22:02:33 +0300

    Initial commit

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ContactList = ({ contacts, onDeleteContact }) => {
+  return (
+    <ul>
+      {contacts.map(({ id, name, number }) => (
+        <li key={id}>
+          {name}: {number}
+          <button onClick={() => onDeleteContact(id)}>Delete</button>
+        </li>
+      ))}
+    </ul>
+  )
+};
+
+ContactList.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  number: PropTypes.string.isRequired,
+  onDeleteContact: PropTypes.func.isRequired,
+};
+
+export default ContactList;
